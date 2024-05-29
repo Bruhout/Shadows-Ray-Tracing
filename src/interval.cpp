7093d85c39edb74d8d2916c07a943d42a3886b0b
Math_Amphetamine <147948392+Bruhout@users.noreply.github.com>
2024-05-29 21:12:33 +0530

    Create interval.cpp

---
+++ src/interval.cpp
@@ -0,0 +1,34 @@
+#include "../header/interval.hpp"
+#include <random>
+
+Interval::Interval()
+    : min(-1000000),
+      max(1000000)
+{/*Defaults to between -ve one million and +ve one million*/
+
+}
+
+Interval::Interval(float Min , float Max)
+    : min(Min),
+      max(Max)
+{}
+
+float Interval::GetSize()
+{
+    return max - min;
+}
+
+bool Interval::Contains(float Value)
+{
+    if (Value > max)
+        return false;
+    else if (Value < min)
+        return false;
+    else
+        return true;
+}
+
+float Interval::RandNum()
+{
+    return dis(gen);
+}
